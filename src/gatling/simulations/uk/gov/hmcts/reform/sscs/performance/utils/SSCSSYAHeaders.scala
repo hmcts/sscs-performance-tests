566cfcfa6f982cecca3685e60c999e8d3bf15c80
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2020-11-10 12:52:01 +0000

    Update SSCSSYAHeaders.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/sscs/performance/utils/SSCSSYAHeaders.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/sscs/performance/utils/SSCSSYAHeaders.scala
@@ -89,4 +89,31 @@ val headers_0 = Map(
 		"Sec-Fetch-Site" -> "same-origin",
 		"Sec-Fetch-User" -> "?1",
 		"Upgrade-Insecure-Requests" -> "1")
+
+	val headers_17 = Map(
+		"Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+		"Accept-Encoding" -> "gzip, deflate, br",
+		"Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+		"Sec-Fetch-Dest" -> "document",
+		"Sec-Fetch-Mode" -> "navigate",
+		"Sec-Fetch-Site" -> "same-site",
+		"Sec-Fetch-User" -> "?1",
+		"Upgrade-Insecure-Requests" -> "1",
+		"User-Agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36",
+		"sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+		"sec-ch-ua-mobile" -> "?0")
+
+	val headers_18 = Map(
+		"Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+		"Accept-Encoding" -> "gzip, deflate, br",
+		"Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+		"Origin" -> "https://idam-web-public.perftest.platform.hmcts.net",
+		"Sec-Fetch-Dest" -> "document",
+		"Sec-Fetch-Mode" -> "navigate",
+		"Sec-Fetch-Site" -> "same-origin",
+		"Sec-Fetch-User" -> "?1",
+		"Upgrade-Insecure-Requests" -> "1",
+		"User-Agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36",
+		"sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+		"sec-ch-ua-mobile" -> "?0")
 }
