65e3b684c1d9a9c669273b0cb165c0bf9b415f87
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2020-11-11 12:06:41 +0000

    Update AppealingReason.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/sscs/performance/submityourappeal/AppealingReason.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/sscs/performance/submityourappeal/AppealingReason.scala
@@ -26,6 +26,19 @@ object AppealingReason{
     "sec-fetch-site" -> "same-origin",
     "sec-fetch-user" -> "?1",
     "upgrade-insecure-requests" -> "1")
+  
+ val headers_sign_out = Map(
+		"Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+		"Accept-Encoding" -> "gzip, deflate, br",
+		"Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+		"Sec-Fetch-Dest" -> "document",
+		"Sec-Fetch-Mode" -> "navigate",
+		"Sec-Fetch-Site" -> "same-origin",
+		"Sec-Fetch-User" -> "?1",
+		"Upgrade-Insecure-Requests" -> "1",
+		"User-Agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36",
+		"sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+		"sec-ch-ua-mobile" -> "?0")
 
 
   // =======================================================================================
@@ -174,6 +187,14 @@ val attendHearing=
     .check(regex("Appeal submitted"))
   )
     .pause(thinktime)
+  
+  val logout = 
+  exec(http("TX36_SSCS_SignOut")
+  .get("/sign-out")
+  .headers(header_sign_out)
+  .check(status.in(200,302)))
+
+
 
 
 
