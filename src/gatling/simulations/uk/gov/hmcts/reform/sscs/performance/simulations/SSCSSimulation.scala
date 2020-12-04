16adf60497a9cd465c69326a178aa67892f6bf57
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2020-12-04 14:09:48 +0000

    Update SSCSSimulation.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/sscs/performance/simulations/SSCSSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/sscs/performance/simulations/SSCSSimulation.scala
@@ -53,7 +53,8 @@ class SSCSSimulation extends Simulation {
   setUp(
     scenarioSYA.inject(atOnceUsers(1)).protocols(httpProtocolSYA),
     scenarioSSCSCORNoUpload.inject(atOnceUsers(1)).protocols(httpProtocolTYA),
-    scenarioSSCSCORWithUpload.inject(atOnceUsers(1)).protocols(httpProtocolTYA))
+    scenarioSSCSCORWithUpload.inject(atOnceUsers(1)).protocols(httpProtocolTYA)
+    .assertions(global.successfulRequests.percent.is(100))
 
 }
 
